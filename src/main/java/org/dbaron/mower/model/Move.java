fea864fb50c2ce9bc76c0337ed0292ea44215ff1
Damien BARON <damien@baron.fr>
2015-01-31 20:25:50 +0100

    Adding contructor for Move class

--- src/main/java/org/dbaron/mower/model/Move.java
+++ src/main/java/org/dbaron/mower/model/Move.java
@@ -3,8 +3,15 @@ package org.dbaron.mower.model;
 /**
  * Created by dbaron on 27/01/15.
  */
-public enum Move {
-    A,
-    D,
-    G
+public class Move {
+
+    private String code;
+
+    public Move(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
